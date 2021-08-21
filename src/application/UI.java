9bf233ef671090b440fea5f99bc9a9aafdf26c75
Lucas Lemes <lucalem0309@gmail.com>
2021-08-20 23:03:40 -0300

    Chess layer and printed the board

---
+++ src/application/UI.java
@@ -0,0 +1,34 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+
+	public static void printBoard(ChessPiece[][] pieces) {
+		String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h"};
+		
+		for (int i = 0; i < pieces.length; i++) {
+			
+			System.out.print((8 - i) + " ");
+			
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.print(" ");
+		for (String l : letters) {
+			System.out.print(" " + l);
+		}
+
+	}
+
+	private static void printPiece(ChessPiece piece) {
+		if (piece == null) {
+			System.out.print("-");
+		} else {
+			System.out.print(piece);
+		}
+		System.out.print(" ");
+	}
+}
