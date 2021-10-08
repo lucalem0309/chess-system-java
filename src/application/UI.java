ddd050f2592e94faf71577a218a70691026fe426
Lucas Lemes <lucalem0309@gmail.com>
2021-10-07 21:39:29 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,7 +55,27 @@ public class UI {
 			System.out.print((8 - i) + " ");
 
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+		System.out.print(" ");
+		for (String l : letters) {
+			System.out.print(" " + l);
+		}
+		
+		System.out.println();
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h" };
+
+		for (int i = 0; i < pieces.length; i++) {
+
+			System.out.print((8 - i) + " ");
+
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
@@ -67,9 +87,12 @@ public class UI {
 		System.out.println();
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
